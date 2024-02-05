dbd4ad6fcd113cd07695d3523c135b3bf7778239
OttJoh94 <145072357+OttJoh94@users.noreply.github.com>
2024-02-05 17:22:03 +0100

    fix model names

--- SuperheroSocialClub/Database/SuperheroRepo.cs
+++ SuperheroSocialClub/Database/SuperheroRepo.cs
@@ -2,12 +2,12 @@
 
 namespace SuperheroSocialClub.Database
 {
-	public class SuperheroRepo
-	{
-		public static List<SuperheroModel> Superheroes { get; set; } = new()
-		{
-			new SuperheroModel {Id = 1, Name = "Otto", SecretIdentity = "Wigwen" , Superpower = "Thresh" , Image = "~/images/superhero1.jpg"},
-			new SuperheroModel {Id = 2, Name = "Gustav", SecretIdentity = "CatRalf" , Superpower = "Pantheon" , Image = "~/images/superhero1.jpg2"}
-		};
-	}
+    public class SuperheroRepo
+    {
+        public static List<SuperheroModel> Superheroes { get; set; } = new()
+        {
+            new SuperheroModel {Id = 1, Name = "Otto", SecretIdentity = "Wigwen" , Superpower = "Thresh" , Image = "~/images/superhero1.jpg"},
+            new SuperheroModel {Id = 2, Name = "Gustav", SecretIdentity = "CatRalf" , Superpower = "Pantheon" , Image = "~/images/superhero1.jpg2"}
+        };
+    }
 }
--- SuperheroSocialClub/Pages/Index.cshtml.cs
+++ SuperheroSocialClub/Pages/Index.cshtml.cs
@@ -1,20 +1,12 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SuperheroSocialClub.Pages
 {
-	public class IndexModel : PageModel
-	{
-		private readonly ILogger<IndexModel> _logger;
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
 
-		public IndexModel(ILogger<IndexModel> logger)
-		{
-			_logger = logger;
-		}
-
-		public void OnGet()
-		{
-
-		}
-	}
+        }
+    }
 }
