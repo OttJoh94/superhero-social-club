1f76698e17d4e4145c3a865f5bc2caa9491aabd7
OttJoh94 <145072357+OttJoh94@users.noreply.github.com>
2024-02-05 16:23:11 +0100

    add models and repo

---
+++ SuperheroSocialClub/Database/SuperheroRepo.cs
@@ -0,0 +1,13 @@
+﻿using SuperheroSocialClub.Models;
+
+namespace SuperheroSocialClub.Database
+{
+	public class SuperheroRepo
+	{
+		public static List<SuperheroModel> Superheroes { get; set; } = new()
+		{
+			new SuperheroModel {Id = 1, Name = "Otto", SecretIdentity = "Wigwen" , Superpower = "Thresh" , Image = "~/images/superhero1.jpg"},
+			new SuperheroModel {Id = 2, Name = "Gustav", SecretIdentity = "CatRalf" , Superpower = "Pantheon" , Image = "~/images/superhero1.jpg2"}
+		};
+	}
+}
---
+++ SuperheroSocialClub/Models/SuperheroModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SuperheroSocialClub.Models
+{
+	public class Superhero
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = null!;
+		public string? SecretIdentity { get; set; }
+		public string? Superpower { get; set; }
+		public string? Image { get; set; }
+	}
+}
