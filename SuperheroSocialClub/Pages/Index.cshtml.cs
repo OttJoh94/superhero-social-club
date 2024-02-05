8b1a4d0f4dc365e243b3134de5843404999e368d
OttJoh94 <olg.johansson@gmail.com>
2024-02-05 23:27:52 +0100

    add cards on index and add and edit page

--- SuperheroSocialClub/Pages/Index.cshtml.cs
+++ SuperheroSocialClub/Pages/Index.cshtml.cs
@@ -1,12 +1,15 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SuperheroSocialClub.Database;
+using SuperheroSocialClub.Models;
 
 namespace SuperheroSocialClub.Pages
 {
     public class IndexModel : PageModel
     {
+        public List<SuperheroModel>? Superheroes { get; set; }
         public void OnGet()
         {
-
+            Superheroes = SuperheroRepo.Superheroes;
         }
     }
 }
